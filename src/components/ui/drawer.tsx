871f78e6bf48b7bb866fb17e7ae37bff4781952f
warjamma <lgold141@gmail.com>
2025-07-28 16:16:29 +0700

    fix: build

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,8 +1,13 @@
 "use client"
 
 import * as React from "react"
+import {
+  type FC,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+  forwardRef
+} from "react"
 import { Drawer as DrawerPrimitive } from "vaul"
-
 import { cn } from "@/lib/utils"
 
 const Drawer = ({
@@ -16,16 +21,20 @@ const Drawer = ({
 )
 Drawer.displayName = "Drawer"
 
-const DrawerTrigger = DrawerPrimitive.Trigger
+const DrawerTrigger: FC<ComponentPropsWithoutRef<typeof DrawerPrimitive.Trigger>> =
+  DrawerPrimitive.Trigger
 
-const DrawerPortal = DrawerPrimitive.Portal
+const DrawerPortal: FC<ComponentPropsWithoutRef<typeof DrawerPrimitive.Portal>> =
+  DrawerPrimitive.Portal
 
-const DrawerClose = DrawerPrimitive.Close
+const DrawerClose: FC<ComponentPropsWithoutRef<typeof DrawerPrimitive.Close>> =
+  DrawerPrimitive.Close
 
-const DrawerOverlay = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Overlay>
->(({ className, ...props }, ref) => (
+const DrawerOverlay: FC<
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Overlay> & {
+    ref?: React.Ref<ElementRef<typeof DrawerPrimitive.Overlay>>
+  }
+> = forwardRef(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
     className={cn("fixed inset-0 z-50 bg-black/80", className)}
@@ -34,10 +43,11 @@ const DrawerOverlay = React.forwardRef<
 ))
 DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName
 
-const DrawerContent = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+const DrawerContent: FC<
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Content> & {
+    ref?: React.Ref<ElementRef<typeof DrawerPrimitive.Content>>
+  }
+> = forwardRef(({ className, children, ...props }, ref) => (
   <DrawerPortal>
     <DrawerOverlay />
     <DrawerPrimitive.Content
@@ -59,10 +69,7 @@ const DrawerHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn("grid gap-1.5 p-4 text-center sm:text-left", className)}
-    {...props}
-  />
+  <div className={cn("grid gap-1.5 p-4 text-center sm:text-left", className)} {...props} />
 )
 DrawerHeader.displayName = "DrawerHeader"
 
@@ -70,32 +77,28 @@ const DrawerFooter = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn("mt-auto flex flex-col gap-2 p-4", className)}
-    {...props}
-  />
+  <div className={cn("mt-auto flex flex-col gap-2 p-4", className)} {...props} />
 )
 DrawerFooter.displayName = "DrawerFooter"
 
-const DrawerTitle = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Title>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>
->(({ className, ...props }, ref) => (
+const DrawerTitle: FC<
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Title> & {
+    ref?: React.Ref<ElementRef<typeof DrawerPrimitive.Title>>
+  }
+> = forwardRef(({ className, ...props }, ref) => (
   <DrawerPrimitive.Title
     ref={ref}
-    className={cn(
-      "text-lg font-semibold leading-none tracking-tight",
-      className
-    )}
+    className={cn("text-lg font-semibold leading-none tracking-tight", className)}
     {...props}
   />
 ))
 DrawerTitle.displayName = DrawerPrimitive.Title.displayName
 
-const DrawerDescription = React.forwardRef<
-  React.ElementRef<typeof DrawerPrimitive.Description>,
-  React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Description>
->(({ className, ...props }, ref) => (
+const DrawerDescription: FC<
+  ComponentPropsWithoutRef<typeof DrawerPrimitive.Description> & {
+    ref?: React.Ref<ElementRef<typeof DrawerPrimitive.Description>>
+  }
+> = forwardRef(({ className, ...props }, ref) => (
   <DrawerPrimitive.Description
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
@@ -114,5 +117,5 @@ export {
   DrawerHeader,
   DrawerFooter,
   DrawerTitle,
-  DrawerDescription,
+  DrawerDescription
 }
